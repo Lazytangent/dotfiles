ba6a599d1bb5f95e9d8044d98ad512b09b15f46a
Peter Mai <pm123633@gmail.com>
2021-11-25 13:39:27 -0600

    Update hammerspoon config

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -1,5 +1,21 @@
+local spaces = require("hs._asm.undocumented.spaces")
+
+local function dump(o)
+  if type(o) == 'table' then
+    local s = "{ "
+    for k,v in pairs(o) do
+      if type(k) ~= 'number' then k = '"'..k..'"' end
+      s = s .. '['..k..'] = ' .. dump(v) .. ','
+    end
+    return s ..'} '
+  else
+    return tostring(o)
+  end
+end
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({ title="Hammerspoon", informativeText="Hello World" }):send()
+  space_id = spaces.activeSpace()
+  print(dump(spaces.query()))
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
@@ -9,3 +25,17 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
   f.x = f.x - 10
   win:setFrame(f)
 end)
+
+function reloadConfig(files)
+  doReload = false
+  for _, file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
