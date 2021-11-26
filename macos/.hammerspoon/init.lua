82480a2208e5bd485cfa67f2ae5c788bda628f1a
Lazytangent <pm123633@gmail.com>
2021-11-25 19:23:41 -0600

    Update hammerspoon config

--- macos/.hammerspoon/init.lua
+++ macos/.hammerspoon/init.lua
@@ -1,4 +1,22 @@
 local spaces = require("hs._asm.undocumented.spaces")
+hhtwm = require('hhtwm')
+
+local isMenubarVisible = hs.screen.primaryScreen():frame().y > 0
+
+local fullMargin = 12
+local halfMargin = fullMargin / 2
+
+local screenMargin = {
+  top = (isMenubarVisible and 22 or 0) + halfMargin,
+  bottom = halfMargin,
+  left = halfMargin,
+  right = halfMargin,
+}
+
+hhtwm.margin = fullMargin
+hhtwm.screenMargin = screenMargin
+
+hhtwm.start()
 
 local function dump(o)
   if type(o) == 'table' then
@@ -18,11 +36,24 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   print(dump(spaces.query()))
 end)
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "E", function()
+  print(dump(hhtwm.getLayout()))
+  hhtwm.setLayout('tabbed-left')
+end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
 
   f.x = f.x - 10
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x + 10
   win:setFrame(f)
 end)
 
@@ -37,5 +68,7 @@ function reloadConfig(files)
     hs.reload()
   end
 end
+
+hhtwm.setLayout('tabbed-left')
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
