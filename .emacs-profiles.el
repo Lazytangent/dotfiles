33e4662ff79f7975abe29ff7ac8d9404350192a5
Peter Mai <pm123633@gmail.com>
2021-12-05 12:39:51 -0600

    Update profiles

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,3 @@
 (("default" . ((user-emacs-directory . "~/.config/doom-emacs")))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")))
  ("custom" . ((user-emacs-directory . "~/.config/custom-emacs"))))
