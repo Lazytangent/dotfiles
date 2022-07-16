8e126d0e40e830dd656ef454b94123418340db26
Lazytangent <peter.tn.mai@gmail.com>
2022-07-16 16:04:53 -0500

    Set DOOMDIR

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,5 @@
 (("default" . ((user-emacs-directory . "~/.config/doom-emacs")))
- ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")
+            (env . (("DOOMDIR" . "~/.config/doom-emacs")))))
  ("custom" . ((user-emacs-directory . "~/.config/custom-emacs")))
  ("vanilla" . ((user-emacs-directory . "~/.config/vanillamacs"))))
